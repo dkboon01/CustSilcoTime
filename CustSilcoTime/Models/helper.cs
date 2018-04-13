e99af99ee5ba080156dfc71f9b25aba8c7f6e18f
dkboon01 <boonedkw@gmail.com>
2018-04-13 15:56:11 -0400

    helperadded

---
+++ CustSilcoTime/Models/helper.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Mvc;
+
+using System.Net;
+using System.IO;
+using System.Configuration;
+using System.Data;
+using System.Collections;
+//using SilcoSedonaCustomApi;
+
+using System.Data.Entity;
+
+using System.Data.SqlClient;
+//using CustSilcoPay.Models;
+using System.Globalization;
+
+
+namespace CustSilcoTime.Models
+{
+}
