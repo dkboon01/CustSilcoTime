4dfaaccfabbff3f40de09536a70dbeec1caadb87
Diana Boone <boonedkw@gmail.com>
2018-04-16 10:46:24 -0400

    GetTimeSheet

--- CustSilcoTime/Controllers/ValuesController.cs
+++ CustSilcoTime/Controllers/ValuesController.cs
@@ -4,36 +4,49 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using CustSilcoTime.Models;
 
 namespace CustSilcoTime.Controllers
 {
     public class ValuesController : ApiController
     {
-        // GET api/values
-        public IEnumerable<string> Get()
+        private Cust_Silco_PYEntities db = new Cust_Silco_PYEntities();
+
+        [Route("api/Values/GetTimeSheetManagerPayweek/{manager}/{payweek}")]
+        public HttpResponseMessage GetTimeSheetForManagerPayweek(string manager, int payweek)
         {
-            return new string[] { "value1", "value2" };
+
+            var timesheet =
+                       Helper.GetTimeSheetForManagerPayweek<SelectTimeSheetForManagerPayweek_Result>(manager, payweek).ToList();
+
+
+            return Request.CreateResponse(HttpStatusCode.OK, timesheet);
         }
+        //// GET api/values
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
 
         // GET api/values/5
-        public string Get(int id)
-        {
-            return "value";
-        }
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
 
-        // POST api/values
-        public void Post([FromBody]string value)
-        {
-        }
+        //// POST api/values
+        //public void Post([FromBody]string value)
+        //{
+        //}
 
-        // PUT api/values/5
-        public void Put(int id, [FromBody]string value)
-        {
-        }
+        //// PUT api/values/5
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
 
-        // DELETE api/values/5
-        public void Delete(int id)
-        {
-        }
+        //// DELETE api/values/5
+        //public void Delete(int id)
+        //{
+        //}
     }
 }
--- CustSilcoTime/Models/helper.cs
+++ CustSilcoTime/Models/helper.cs
@@ -10,15 +10,27 @@ using System.IO;
 using System.Configuration;
 using System.Data;
 using System.Collections;
-//using SilcoSedonaCustomApi;
+
 
 using System.Data.Entity;
 
 using System.Data.SqlClient;
-//using CustSilcoPay.Models;
+
 using System.Globalization;
 
 
 namespace CustSilcoTime.Models
 {
+    public class Helper
+    {
+        public static List<SelectTimeSheetForManagerPayweek_Result> GetTimeSheetForManagerPayweek<T>( string manager, int payweek)
+
+        where T : class
+        {
+            var db = new Cust_Silco_PYEntities();
+            List<SelectTimeSheetForManagerPayweek_Result> EmployeeEntry = db.SelectTimeSheetForManagerPayweek(manager, payweek).ToList();
+            return EmployeeEntry;
+
+        }
+    }
 }
